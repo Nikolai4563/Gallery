ca5fa547959d51f01a1eec0694ad9a1c3cbe594f
Nikolai4563 <aspirework@mail.ru>
2013-08-22 16:08:44 +0300

    fixed

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-ActionController::Base.cache_store = :file_store, "#{Rails.root}/public/cache"
+#ActionController::Base.cache_store = :file_store, "#{Rails.root}/public/cache"
 Gallery::Application.initialize!
